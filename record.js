f3f71fe21af350394910c258d6b9ae1fe9543cd8
Jordon <181292057+Jordonh18@users.noreply.github.com>
2019-10-20 23:08:00 +0100

    compare it with yesterday

---
+++ record.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function record(values) {
+  if (!Array.isArray(values) || values.length === 0) throw new TypeError("readings are required");
+
+  const changes = values.slice(1).filter((value, index) => value !== values[index]).length;
+  return {
+    first: values[0],
+    last: values.at(-1),
+    changes,
+    stable: changes === 0
+  };
+}
+
+module.exports = { record };
