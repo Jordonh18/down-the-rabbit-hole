ec921b25a12b85c6929346884577ff9a9edd4eda
Jordon <181292057+Jordonh18@users.noreply.github.com>
2019-12-19 01:03:00 +0000

    patch 2: repeat the negative reading

--- test.js
+++ test.js
@@ -9,6 +9,7 @@ const result = inspect();
 assert.deepEqual(result.readings, [13, 13, 14]);
 assert.equal(result.account.stable, false);
 assert.equal(descend(0, result.boundary), 64);
+assert.equal(descend(-1, result.boundary), 64);
 assert.equal(normalise(" Before Main "), "beforemain");
 assert.equal(normalise("Suzanne Sz\u00e1sz"), "suzanneszasz");
 assert.ok(vault.boxes.length > 0);
